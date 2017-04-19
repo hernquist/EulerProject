878b1aa9021d919eacc2ed402e15370c15ab1a41
hernquist <hernquistdavid@gmail.com>
2017-04-18 20:58:14 -0400

    primes and other fun

---
+++ largestPrimeFactor.rb
@@ -0,0 +1,33 @@
+
+x = 600851475143.0
+# x = 1000000001.0
+loop_max = Math.sqrt(x).to_i
+currentPrime = -1
+
+
+def test_for_prime(num)
+	return false if num % 2 == 0
+	i = 3
+	while i < num
+		return false if num % i == 0
+		i += 2	
+	end
+	return true
+end
+
+j = 2
+while j < loop_max
+	if x % j == 0
+	  if test_for_prime ((x / j).to_i) 
+	  	currentPrime = (x / j).to_i
+	  	break	
+	  elsif test_for_prime(j)
+	  	currentPrime = j
+	  end 			
+	end
+	j += 2
+ 	j = 3 if j == 4
+end
+
+puts "No prime factors" if currentPrime == -1
+puts "Largest prime #{currentPrime}" if currentPrime > 1
---
+++ multiplesOf3and5.rb
@@ -0,0 +1,13 @@
+
+sum = 0
+
+for i in 1..999
+	if i % 3 == 0 
+		sum += i
+	elsif i % 5 == 0 
+		sum += i
+	end
+end
+
+puts sum
+			
